a3c00c1f0a753258ab96dd581fff22207b207014
rodrigocqb <rodrigocortibarros@gmail.com>
2024-12-01 15:38:39 -0300

    fix: export custom jwt guard file instead of passport version

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,4 @@ export * from './auth/auth.module';
 export * from './auth/jwt.strategy';
 export * from './auth/permissions/permissions.decorator';
 export * from './auth/permissions/permissions.guard';
-export { AuthGuard } from '@nestjs/passport';
+export { JwtAuthGuard } from './auth/jwt-auth.guard';
