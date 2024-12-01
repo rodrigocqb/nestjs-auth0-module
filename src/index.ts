5c00ba4bab82d6189a3eb9c2522c194f72b0aa28
rodrigocqb <rodrigocortibarros@gmail.com>
2024-12-01 15:00:32 -0300

    feat: add AuthGuard export on index file

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export * from './auth/auth.module';
 export * from './auth/jwt.strategy';
 export * from './auth/permissions/permissions.decorator';
 export * from './auth/permissions/permissions.guard';
+export { AuthGuard } from '@nestjs/passport';
